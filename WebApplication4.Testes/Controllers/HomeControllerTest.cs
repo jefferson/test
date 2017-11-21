3ee3064d7ac41f4a6a5cd233bca3999d374dd78e
jefferson <jefferson_alvespereira@yahoo.com.br>
2017-11-21 20:35:00 -0200

    inciando projeto

---
+++ WebApplication4.Testes/Controllers/HomeControllerTest.cs
@@ -0,0 +1,25 @@
+﻿using System.Web.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WebApplication4;
+using WebApplication4.Controllers;
+
+namespace WebApplication4.Testes.Controllers
+{
+    [TestClass]
+    public class HomeControllerTest
+    {
+        [TestMethod]
+        public void Index()
+        {
+            // Organizar
+            HomeController controller = new HomeController();
+
+            // Agir
+            ViewResult result = controller.Index() as ViewResult;
+
+            // Declarar
+            Assert.IsNotNull(result);
+            Assert.AreEqual("Home Page", result.ViewBag.Title);
+        }
+    }
+}
