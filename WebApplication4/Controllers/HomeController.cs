a0f1689e1e18d3afdaf62e27b008954fc5b47364
Jefferson <jefferson_alvespereira@yahoo.com.br>
2017-11-22 07:29:28 -0200

    atualizando títutlo

--- WebApplication4/Controllers/HomeController.cs
+++ WebApplication4/Controllers/HomeController.cs
@@ -10,7 +10,7 @@ namespace WebApplication4.Controllers
     {
         public ActionResult Index()
         {
-            ViewBag.Title = "Home Page Teste";
+            ViewBag.Title = "Home Page";
 
             return View();
         }
